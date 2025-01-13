1c67c9ab3a28f22b92e3c065f81f918ef86d8c71
Heiko Sippel <heiko.sippel@infaktum.de>
2025-01-13 18:47:16 +0100

    Perzeptron

--- Hopfield-Netze/Hopfield.py
+++ Hopfield-Netze/Hopfield.py
@@ -11,16 +11,23 @@ class Hopfield():
                 self.W[i,j] = sum([X[k][i] * X[k][j] for k in range(len(X))]) / self.n
         np.fill_diagonal(self.W, 0)
 
-    def recall(self, x, steps=100):
+    def train(self, patterns):
+        for pattern in patterns:
+            flat_pattern = pattern.flatten()
+            self.W += np.outer(flat_pattern, flat_pattern)
+        np.fill_diagonal(self.W, 0)
+        self.W /= len(patterns)
+
+    def recall(self, x, steps=1000):
         x_flat = x.flatten()
         for _ in range(steps):
             x_flat = np.sign(self.W @ x_flat)
-        return x_flat.reshape(4)
+        return x_flat.reshape(x.shape)
 
 if __name__ == "__main__":
     X = np.array([[1, 1, -1, -1], [-1, 1, 1, -1], [-1, -1, -1, 1]])
     hopfield = Hopfield(4)
-    hopfield.fit(X)
+    hopfield.train(X)
     print(hopfield.W)
 
-    print(hopfield.recall(np.array([1, 1, -1, -1])))
+    print(hopfield.recall(np.array([1, 1, 1, 1])))
